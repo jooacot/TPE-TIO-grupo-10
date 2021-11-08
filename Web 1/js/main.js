d3946962e6b249a4d9e523a94a770c8da9bfd9d3
Santiago Salomon <santisalomon@outlook.es>
2021-11-08 12:54:06 -0300

    agrego captura de mi trabajo

--- Web 1/js/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-document.querySelector("#btn-nav").addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-    document.querySelector("header nav ul").classList.toggle("show");
-}
